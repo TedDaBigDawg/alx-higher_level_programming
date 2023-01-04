490d5e73889ab178edacad05c18c39b058ae4929
Ted Osiobe <osiobeted@gmail.com>
2023-01-04 10:03:18 -0800

    MMM

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -61,9 +61,9 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-        [rect.append('#') for j in range(self.__width)]
-        if i != self.__height - 1:
-        rect.append("\n")
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
         return ("".join(rect))
 
     def __repr__(self):
